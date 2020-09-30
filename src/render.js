7905593374fe3046f40768577fe70e3a9b576377
Jake <jposner91@gmail.com>
2020-09-30 16:57:10 -0400

    complete functionality achieved. can now record a video from one window on the screen and save it. plenty of bugs I'm sure and probably some type of optimization, but this was a cool little project.

--- src/render.js
+++ src/render.js
@@ -1,10 +1,40 @@
 const { desktopCapturer, remote } = require('electron')
-const { Menu } = remote
+const { writeFile } = require('fs')
+const { Menu, dialog } = remote
+
 const startButton = document.getElementById("startButton")
 const stopButton = document.getElementById("stopButton")
 const videoSelectButton = document.getElementById("videoSelectButton")
 videoSelectButton.onclick = getVideoSources
+startButton.onclick = startRecording
+stopButton.onclick = stopRecording
+
 const video = document.querySelector("video")
+let mediaPlayer
+var mediaRecorder
+const recordedChunks = []
+
+function startRecording() {
+    if (mediaRecorder === undefined) {
+        console.log("undefined media recorder")
+    } else {
+        console.log("start recording")
+        mediaRecorder.start();
+        startButton.classList.add('is-danger');
+        startButton.innerText = 'Recording';
+    }
+}
+
+function stopRecording() {
+    if (mediaRecorder === undefined) {
+        console.log("undefined media recorder")
+    } else {
+        console.log("stop recording")
+        mediaRecorder.stop();
+        startButton.classList.remove('is-danger');
+        startButton.innerText = 'Start';
+    }
+}
 
 async function getVideoSources() {
     const inputSources = await desktopCapturer.getSources({
@@ -12,7 +42,7 @@ async function getVideoSources() {
     })
 
     const videoOptionsMenu = Menu.buildFromTemplate(
-        inputSources.map(source => {
+        inputSources.map((source) => {
             return {
                 label: source.name,
                 click: () => selectVideoSource(source)
@@ -23,7 +53,7 @@ async function getVideoSources() {
 }
 
 async function selectVideoSource(source) {
-    videoSelectButton.text = source.name
+    videoSelectButton.innerText = source.name
 
     const constraints = {
         audio:false,
@@ -40,4 +70,34 @@ async function selectVideoSource(source) {
 
     video.srcObject = stream
     video.play()
+
+    const options = { mimeType: 'video/webm; codecs=vp9' }
+    mediaRecorder = new MediaRecorder(stream, options)
+    mediaRecorder.ondataavailable = handleDataAvailable
+    mediaRecorder.onstop = saveVideoChunksOnStop
+}
+
+function handleDataAvailable(e) {
+    console.log("video data available: " + e.data)
+    recordedChunks.push(e.data)
+}
+
+async function saveVideoChunksOnStop(e) {
+    console.log("saveVideoFromChunks() running")
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    })
+
+    const buffer = Buffer.from(await blob.arrayBuffer())
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: "Save video",
+        defaultPath: `vid-${Date.now()}.webm`
+    })
+    if(filePath !== undefined && filePath !== "") {
+        console.log(filePath)
+
+        writeFile(filePath, buffer, () => console.log("video saved successfully!"))
+    } else {
+        console.log("Operation terminated, file not saved..")
+    }
 }
